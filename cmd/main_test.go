45e36cf72f2d27dde265615274db960fa09efd38
Luiz Fernando <luizgomes03f@gmail.com>
2025-08-24 21:17:46 -0300

    adding test do ping route

---
+++ cmd/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
+
+func TestPingEndpoint(t *testing.T) {
+	router := setupRouter()
+
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Contains(t, w.Body.String(), "pong")
+}
